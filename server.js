0f613d7a5138b664637a888b4e713ab3c7bad9fa
MustafaGaber1 <mostafagaber657@gmail.com>
2025-04-29 21:52:50 +0300

    server initialized

---
+++ server.js
@@ -0,0 +1,45 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { OpenAI } from "openai";
+
+dotenv.config();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "Hello from Simple AI",
+  });
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+    });
+    console.log("OpenAI Response:", response);
+
+    res.status(200).send({
+      bot: response.choices[0].message.content,
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .send({ error: "An error occurred while processing your request." });
+  }
+});
+
+app.listen(5000, () =>
+  console.log("Server is running on port http://localhost:5000")
+);
